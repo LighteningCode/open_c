033c818450dcffe8dc9e096e210a5079d971048e
Elvis Agbesi <elvisagbesi@gmail.com>
2020-12-22 00:43:08 +0000

    feat: :construction: Main header built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import Sidenav from './components/Sidenav';
 
+function MainHeader() {
+  return (
+    <section className="px-7 py-3 bg-white w-full flex flex-row justify-between">
+      <h3 className="self-center font-bold">OpenC</h3>
+      <input
+        type="text"
+        value={null}
+        style={{ width: "350px" }}
+        className="bg-gray-200 rounded-md py-0.5 px-3" />
+      <i className="fa fa-bell self-center text-gray-700"></i>
+
+      <div className="flex flex-row self-center">
+        <p className="mr-3 text-xs self-center font-bold text-gray-600">Elvis Agbesi</p>
+        <img src="./me.jpg" width="30" height="30" className="rounded-full" />
+      </div>
+    </section>
+  )
+}
+
 function App() {
   return (
-    <main className="flex flex-row w-full h-full">
+    <main className="flex flex-row w-full h-full bg-gray-200">
       <Sidenav />
-      <div></div>
+      <div className="flex flex-col w-full">
+        <MainHeader />
+      </div>
     </main>
   );
 }
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,13 +1,13 @@
 
 function SideItem(props) {
     return (
-        <li className="text-white text-xs mb-4 px-3 py-2 rounded-lg transition-all hover:bg-blue-400 cursor-pointer"><i className={`mr-5 fa ${props.icon}`}></i>{props.name}</li>
+        <li className="text-white text-xs mb-4 px-3 py-2 rounded-lg transition-all hover:bg-blue-400 cursor-pointer"><i className={`mr-2 fa ${props.icon}`}></i>{props.name}</li>
     );
 }
 
 function Sidenav() {
     return (
-        <nav className="flex flex-col bg-blue-700 float-left h-full py-5 px-12">
+        <nav className="flex flex-col bg-blue-700 float-left h-full py-12 px-3 w-48">
             <ul className="list-none mt-16">
                 <SideItem name="News" icon="fa-home" />
                 <SideItem name="Topics" icon="fa-book" />
