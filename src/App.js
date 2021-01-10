b154a58354a25eac5c5378876721fd32247a598e
Elvis Agbesi <elvisagbesi@gmail.com>
2021-01-10 13:07:59 +0000

    feat: :construction: Main header fixed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import Sidenav from './components/Sidenav';
 
 function MainHeader() {
   return (
-    <section className="px-7 py-3 bg-white w-full flex flex-row justify-between">
-      <h3 className="self-center font-bold">OpenC</h3>
+    <section className="px-7 py-3 bg-white w-full flex flex-row">
+      <h3 className="self-center text-lg font-bold mr-4">OpenC</h3>
       <input
         type="text"
         value={null}
         placeholder="Search"
         style={{ width: "350px" }}
-        className="bg-gray-200 rounded-md py-0.5 px-3" />
-      <i className="fa fa-bell self-center text-gray-700"></i>
+        className="bg-gray-200 rounded-md py-0.5 px-3 mr-auto" />
 
+      <i className="fa fa-bell self-center text-gray-700 mx-2"></i>
       <div className="flex flex-row self-center">
-        <p className="mr-3 text-xs self-center font-bold text-gray-600">Elvis Agbesi</p>
+        <p className="mr-3 text-lg self-center font-bold text-gray-600">Elvis Agbesi</p>
         <img src="./me.jpg" width="30" height="30" className="rounded-full" />
       </div>
+
     </section>
   )
 }
